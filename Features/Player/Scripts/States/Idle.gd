96e01fd03c02605a7ffa7e91807dee3562a32a44
Hiran Jr <maximosdrr@gmail.com>
2026-01-05 16:59:22 -0300

    feat: add new poolstick controller

--- Features/Player/Scripts/States/Idle.gd
+++ Features/Player/Scripts/States/Idle.gd
@@ -3,29 +3,24 @@ extends State
 class_name PlayerIdleState
 
 @export var player: Player
-
-var table: Table = null
-var poolstick: Poolstick = null
-
-func _ready() -> void:
-	table = player.table
-	poolstick = player.poolstick
 	
 func _init() -> void:
 	self.type = State.Type.IDLE
 	
 func enter(_metadata: Dictionary[Variant, Variant]):
+	var table = player.table
+	var poolstick = player.poolstick
 	player.player_camera.current = true
 	poolstick.poolstick_camera.current = false
 	poolstick.set_camera_target(null)
 	
-	#Change poolstick parent
-	table.poolstick_respawn_marker.remove_child(poolstick)
-	player.add_child(poolstick)
-	
-	#Move poolstick to the right position
-	poolstick.position = Vector3(0.31, -0.132, 0.572)
-	poolstick.rotation_degrees = Vector3(-73.6, 0, 0)
+	if poolstick.get_parent() == table.poolstick_respawn_marker:
+		table.poolstick_respawn_marker.remove_child(poolstick)
+		player.add_child(poolstick)
+		
+		poolstick.position = Vector3(0.31, -0.132, 0.572)
+		poolstick.rotation_degrees = Vector3(-73.6, 0, 0)
+
 
 func process(_delta: float) -> void:
 	if Input.is_action_just_pressed("aim"):
