f3088ecc96f01ef6b1795be82058fec8934f5471
Hiran Jr <maximosdrr@gmail.com>
2026-01-04 21:22:12 -0300

    fix: game physics

--- Features/Balls/Ball.gd
+++ Features/Balls/Ball.gd
@@ -2,10 +2,18 @@ class_name Ball
 extends RigidBody3D
 
 @export var data: BallResource
+@onready var label_3d: Label3D = $Label3D
+
+func _update_label():
+	var message = "Ball bounce: %s\nBall Friction %s" % [physics_material_override.bounce, physics_material_override.friction]
+	label_3d.text = message
+
+func _process(_delta: float) -> void:
+	_update_label()
 
 func _ready() -> void:
 	if data == null:
-		push_warning("BallPhysics has no Ball resource assigned.")
+		push_warning("Ball has no BallResource assigned.")
 		return
 
 	if data.model != null:
@@ -13,38 +21,24 @@ func _ready() -> void:
 
 	mass = data.mass
 	gravity_scale = data.gravity_scale
+
 	linear_damp = data.linear_damp
 	angular_damp = data.angular_damp
-	continuous_cd = data.continous_cd
+	
+	#Should be false. If you turn it into true, frontal collisions will start seem heavy
+	continuous_cd = false
 	can_sleep = data.can_sleep
+	
+	physics_material_override.bounce = data.bounce
+	physics_material_override.friction = data.friction
 
 	add_to_group(Groups.BALL)
 
-func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	_apply_table_friction(state)
-
-func _apply_table_friction(state: PhysicsDirectBodyState3D) -> void:
-	if state.get_contact_count() == 0:
-		return
-
-	var v := state.linear_velocity
-	var v_xz := Vector3(v.x, 0.0, v.z)
-	var speed := v_xz.length()
-
-	if speed < data.stop_speed:
-		state.linear_velocity = Vector3(0.0, v.y, 0.0)
-		return
-
-	var decel := data.roll_drag * state.step
-	var new_speed = max(0.0, speed - decel)
-	var new_v_xz = v_xz.normalized() * new_speed
-
-	state.linear_velocity = Vector3(new_v_xz.x, v.y, new_v_xz.z)
-
-func strike(direction: Vector3, power: float, hit_offset: Vector3 = Vector3.ZERO) -> void:
+func strike(direction: Vector3, power: float, hit_offset_local: Vector3 = Vector3.ZERO) -> void:
 	var dir := Vector3(direction.x, 0.0, direction.z)
 	if dir.length_squared() < 0.0001 or power <= 0.0:
 		return
 
 	var impulse := dir.normalized() * power
-	apply_impulse(impulse, hit_offset)
+	var hit_offset_world := global_transform.basis * hit_offset_local
+	apply_impulse(impulse, hit_offset_world)
--- Features/Balls/BallResource.gd
+++ Features/Balls/BallResource.gd
@@ -5,8 +5,9 @@ class_name BallResource extends Resource
 @export var gravity_scale := 1.0
 @export var linear_damp := 0.3
 @export var angular_damp := 0.3
-@export var continous_cd := true
 @export var can_sleep := true
+@export var friction := 0.4
+@export var bounce := 0.85
 
 @export var roll_drag: float = 0.6
 @export var stop_speed: float = 0.2
