cce31aec9d71e93e3d52778d65f500f4a9980b4d
Hiran Jr <maximosdrr@gmail.com>
2026-01-10 22:32:49 -0300

    feat: add multiplayer

--- Features/Player/Scripts/HeadPivot.gd
+++ Features/Player/Scripts/HeadPivot.gd
@@ -3,20 +3,22 @@ extends Node3D
 
 @export var camera_mount: RemoteTransform3D
 
+var player_body: CharacterBody3D
+
 @export_group("Settings")
 @export var mouse_sensitivity: float = 0.005
 @export var min_pitch: float = -65.0
 @export var max_pitch: float = 60.0
 
 func _ready() -> void:
-	pass
+	var node = get_parent()
+	while node:
+		if node is CharacterBody3D:
+			player_body = node
+			break
+		node = node.get_parent()
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	elif event.is_action_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
 	if Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
 		return
 
@@ -24,16 +26,9 @@ func _unhandled_input(event: InputEvent) -> void:
 		_handle_camera_rotation(event)
 
 func _handle_camera_rotation(event: InputEventMouseMotion) -> void:
-	if owner:
-		owner.rotate_y(-event.relative.x * mouse_sensitivity)
-	else:
-		rotate_y(-event.relative.x * mouse_sensitivity)
-
-	if camera_mount:
-		camera_mount.rotate_x(-event.relative.y * mouse_sensitivity)
-		
-		camera_mount.rotation.x = clamp(
-			camera_mount.rotation.x,
-			deg_to_rad(min_pitch),
-			deg_to_rad(max_pitch)
-		)
+	if player_body:
+		player_body.rotate_y(-event.relative.x * mouse_sensitivity)
+	
+	rotate_x(-event.relative.y * mouse_sensitivity)
+	
+	rotation.x = clamp(rotation.x, deg_to_rad(min_pitch), deg_to_rad(max_pitch))
