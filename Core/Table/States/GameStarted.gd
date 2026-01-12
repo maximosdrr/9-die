48bf3c9a7df6b5ce150b05882a62e70e11a8138e
Hiran Jr <maximosdrr@gmail.com>
2026-01-12 11:50:41 -0300

    fix: cue problem

--- Core/Table/States/GameStarted.gd
+++ Core/Table/States/GameStarted.gd
@@ -8,20 +8,20 @@ func _init() -> void:
 func enter(metadata: Dictionary[Variant, Variant]):
 	var players_ids = metadata.get('players_ids')
 	assert(players_ids != null)
-	
-	var players_container = get_tree().get_nodes_in_group("players_container")[0]
-	
-	assert(players_container != null)
+
 	for player_id in players_ids:
-		var player = _get_player(players_container, player_id)
+		var player: Player = _get_player(player_id)
 		assert(player != null)
-		if multiplayer.is_server():
-			player.game_handler.equip_game_controller(
-					table.game_controller_scene,
-					table.current_table_game
-			)
 		
-func _get_player(players_container: Node3D, player_id: String):
+		player.game_handler.equip_game_controller(
+				table.game_controller_scene,
+				table.current_table_game
+		)
+		
+func _get_player(player_id: String):
+	var players_container = get_tree().get_nodes_in_group("players_container")[0]
+	assert(players_container != null)
+	
 	for node in players_container.get_children():
 		if node is Player and node.name == player_id:
 			return node
--- Features/Player/Scripts/PlayerGameHandler.gd
+++ Features/Player/Scripts/PlayerGameHandler.gd
@@ -9,7 +9,6 @@ func equip_game_controller(controller_scene: PackedScene, table_game: TableGame)
 	unequip_current_controller()
 	
 	var controller_instance = controller_scene.instantiate()
-	
 	controller_instance.set_multiplayer_authority(player.name.to_int())
 	
 	current_controller = controller_instance
@@ -24,6 +23,12 @@ func equip_game_controller(controller_scene: PackedScene, table_game: TableGame)
 	current_controller.setup(owner, table_game)
 
 func unequip_current_controller():
-	if current_controller:
-		current_controller.queue_free()
-		current_controller = null
+	if not current_controller: return
+	
+	var current_controller_parent = current_controller.get_parent()
+	
+	if current_controller_parent:
+		current_controller_parent.remove_child(current_controller)
+	
+	current_controller.queue_free()
+	current_controller = null
