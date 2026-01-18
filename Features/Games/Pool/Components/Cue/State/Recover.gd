698285dd5c8ecf3c112bc108323d2aabc7b8d5b4
Hiran Farias <maximosdrr@gmail.com>
2026-01-18 20:54:53 -0300

    feat: add screen lock

--- Features/Games/Pool/Components/Cue/State/Recover.gd
+++ Features/Games/Pool/Components/Cue/State/Recover.gd
@@ -1,7 +1,10 @@
-class_name CueRecoverState extends State
+class_name CueRecoverState
+extends State
 
 var cue: Cue
 var _timer: SceneTreeTimer
+
+var _block_until_ms: int = 0
 
 func _init():
 	type = State.Type.CUE_RECOVER
@@ -10,6 +13,8 @@ func setup(parent_node: Node3D):
 	cue = parent_node as Cue
 
 func enter(_m):
+	_block_until_ms = Time.get_ticks_msec() + int(cue.post_shot_cooldown * 1000.0)
+
 	cue.create_tween().tween_property(cue, "position:z", cue.ball_radius_offset, 0.2)
 	cue.create_tween().tween_property(cue, "spin_offset", Vector2.ZERO, 0.5)
 
@@ -20,3 +25,9 @@ func enter(_m):
 
 func exit(_m):
 	_timer = null
+
+func handle_input(event: InputEvent) -> void:
+	if Time.get_ticks_msec() < _block_until_ms:
+		if event is InputEventMouseMotion:
+			get_viewport().set_input_as_handled()
+			return
