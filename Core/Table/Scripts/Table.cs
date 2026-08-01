5e595ee93767ed7bf960f0209d8c2dbd5c5a6a60
Hiran Junior <maximosdrr@gmail.com>
2026-08-01 10:59:27 -0300

    add new pool table

--- Core/Table/Scripts/Table.cs
+++ Core/Table/Scripts/Table.cs
@@ -4,100 +4,100 @@ using Godot;
 [GlobalClass]
 public partial class Table : Node3D
 {
-    public Godot.Collections.Array<string> PlayersOnMatch = new();
+	public Godot.Collections.Array<string> PlayersOnMatch = new();
 
-    public Node3D TableGameHandler;
-    public Area3D TableInfluence;
-    public StateMachine StateMachine;
-    public Label3D DebugLabel;
+	public Node3D TableGameHandler;
+	public Area3D TableInfluence;
+	public StateMachine StateMachine;
+	public Label3D DebugLabel;
 
-    [ExportCategory("NetworkConfiguration")]
-    [Export] public bool EnableNetworkTurnSyncronization = true;
+	[ExportCategory("NetworkConfiguration")]
+	[Export] public bool EnableNetworkTurnSyncronization = true;
 
-    private PackedScene _tableGameScene;
-    [Export]
-    public PackedScene TableGameScene
-    {
-        get => _tableGameScene;
-        set
-        {
-            _tableGameScene = value;
-            if (Engine.IsEditorHint())
-                RebuildEditorPreview();
-        }
-    }
+	private PackedScene _tableGameScene;
+	[Export]
+	public PackedScene TableGameScene
+	{
+		get => _tableGameScene;
+		set
+		{
+			_tableGameScene = value;
+			if (Engine.IsEditorHint())
+				RebuildEditorPreview();
+		}
+	}
 
-    private bool _editorBuilding = false;
-    public TableGame CurrentTableGame;
+	private bool _editorBuilding = false;
+	public TableGame CurrentTableGame;
 
-    public void SetCamera(GlobalCamera camera)
-    {
-        CurrentTableGame?.SetCamera(camera);
-    }
+	public void SetCamera(GlobalCamera camera)
+	{
+		CurrentTableGame?.SetCamera(camera);
+	}
 
-    public override void _Ready()
-    {
-        TableGameHandler = GetNode<Node3D>("TableGameHandler");
+	public override void _Ready()
+	{
+		TableGameHandler = GetNode<Node3D>("TableGameHandler");
 
-        if (Engine.IsEditorHint())
-        {
-            RebuildEditorPreview();
-            return;
-        }
+		if (Engine.IsEditorHint())
+		{
+			RebuildEditorPreview();
+			return;
+		}
 
-        TableInfluence = GetNode<Area3D>("TableInfluence");
-        StateMachine = GetNode<StateMachine>("StateMachine");
-        DebugLabel = GetNode<Label3D>("DebugLabel");
+		TableInfluence = GetNode<Area3D>("TableInfluence");
+		StateMachine = GetNode<StateMachine>("StateMachine");
+		DebugLabel = GetNode<Label3D>("DebugLabel");
 
-        SpawnRuntimeGame();
-    }
+		SpawnRuntimeGame();
+	}
 
-    public override void _Process(double delta)
-    {
-        if (CurrentTableGame == null || CurrentTableGame.TurnOwner == null)
-            return;
+	public override void _Process(double delta)
+	{
+		if (CurrentTableGame == null || CurrentTableGame.TurnOwner == null)
+			return;
 
-        DebugLabel.Text = $"Current player turn: {CurrentTableGame.TurnOwner.Name}";
-    }
+		DebugLabel.Text = $"Current player turn: {CurrentTableGame.TurnOwner.Name}";
+	}
 
-    private void SpawnRuntimeGame()
-    {
-        if (TableGameScene == null)
-        {
-            GD.PushError("Table scene is null!");
-            return;
-        }
+	private void SpawnRuntimeGame()
+	{
+		if (TableGameScene == null)
+		{
+			GD.PushError("Table scene is null!");
+			return;
+		}
 
-        var tableGameInstance = TableGameScene.Instantiate();
-        var tableGame = (TableGame)tableGameInstance;
-        tableGame.Setup(this);
+		var tableGameInstance = TableGameScene.Instantiate();
+		var tableGame = (TableGame)tableGameInstance;
+		tableGame.Setup(this);
 
-        TableGameHandler.AddChild(tableGameInstance);
-        CurrentTableGame = tableGame;
-    }
+		TableGameHandler.AddChild(tableGameInstance);
+		CurrentTableGame = tableGame;
+	}
 
-    private void RebuildEditorPreview()
-    {
-        if (TableGameHandler == null)
-            return;
+	private void RebuildEditorPreview()
+	{
+		if (TableGameHandler == null)
+			return;
 
-        if (_editorBuilding)
-            return;
-        _editorBuilding = true;
+		if (_editorBuilding)
+			return;
+		_editorBuilding = true;
 
-        foreach (var c in TableGameHandler.GetChildren())
-            c.QueueFree();
+		foreach (var c in TableGameHandler.GetChildren())
+			c.QueueFree();
 
-        if (TableGameScene != null)
-        {
-            var preview = TableGameScene.Instantiate();
-            preview.Name = "TableGamePreview";
-            TableGameHandler.AddChild(preview);
+		if (TableGameScene != null)
+		{
+			var preview = TableGameScene.Instantiate();
+			preview.Name = "TableGamePreview";
+			TableGameHandler.AddChild(preview);
 
-            if (GetTree() != null && GetTree().EditedSceneRoot != null)
-                preview.Owner = GetTree().EditedSceneRoot;
-        }
+			if (GetTree() != null && GetTree().EditedSceneRoot != null)
+				preview.Owner = GetTree().EditedSceneRoot;
+		}
 
-        _editorBuilding = false;
-    }
+		_editorBuilding = false;
+	}
 }
--- Features/Games/Pool/Components/Tables/PoolGameTable.cs
+++ Features/Games/Pool/Components/Tables/PoolGameTable.cs
@@ -3,59 +3,59 @@ using Godot;
 [GlobalClass]
 public partial class PoolGameTable : StaticBody3D
 {
-    public PoolScoreMonitor ScoreMonitor;
-    public Area3D BallOffMonitor;
-    public AudioStreamPlayer3D BallPocketedAudio;
+	public PoolScoreMonitor ScoreMonitor;
+	public Area3D BallOffMonitor;
+	public AudioStreamPlayer3D BallPocketedAudio;
 
-    private ulong _lastSoundTimeMsec = 0;
-    private const ulong MinSoundInterval = 50;
+	private ulong _lastSoundTimeMsec = 0;
+	private const ulong MinSoundInterval = 50;
 
-    public override void _Ready()
-    {
-        ScoreMonitor = GetNode<PoolScoreMonitor>("ScoreMonitor");
-        BallOffMonitor = GetNode<Area3D>("BallOffMonitor");
-        BallPocketedAudio = GetNode<AudioStreamPlayer3D>("BallPocketed");
+	public override void _Ready()
+	{
+		ScoreMonitor = GetNode<PoolScoreMonitor>("ScoreMonitor");
+		BallOffMonitor = GetNode<Area3D>("BallOffMonitor");
+		BallPocketedAudio = GetNode<AudioStreamPlayer3D>("BallPocketed");
 
-        GetNode<Node3D>("Rails").AddToGroup("Cushion");
-    }
+		GetNode<Node3D>("Rails").AddToGroup("Cushion");
+	}
 
-    private void OnPocketsDetectorsBallEntered(Node3D body)
-    {
-        if (body is not Ball ball)
-            return;
+	private void OnPocketsDetectorsBallEntered(Node3D body)
+	{
+		if (body is not Ball ball)
+			return;
 
-        if ((bool)ball.GetMeta("in_pocket", false))
-            return;
+		if ((bool)ball.GetMeta("in_pocket", false))
+			return;
 
-        ball.SetMeta("in_pocket", true);
+		ball.SetMeta("in_pocket", true);
 
-        CallDeferred(MethodName.ApplyPocketPhysics, ball);
-        EmitBallPocketedSound();
+		CallDeferred(MethodName.ApplyPocketPhysics, ball);
+		EmitBallPocketedSound();
 
-        var timer = GetTree().CreateTimer(1.0);
-        timer.Timeout += () =>
-        {
-            if (IsInstanceValid(ball))
-                ball.SetMeta("in_pocket", false);
-        };
-    }
+		var timer = GetTree().CreateTimer(1.0);
+		timer.Timeout += () =>
+		{
+			if (IsInstanceValid(ball))
+				ball.SetMeta("in_pocket", false);
+		};
+	}
 
-    private void ApplyPocketPhysics(Ball ball)
-    {
-        if (IsInstanceValid(ball))
-        {
-            ball.AngularVelocity = Vector3.Zero;
-            ball.LinearVelocity = new Vector3(0.1f, 0, 0.1f);
-        }
-    }
+	private void ApplyPocketPhysics(Ball ball)
+	{
+		if (IsInstanceValid(ball))
+		{
+			ball.AngularVelocity = Vector3.Zero;
+			ball.LinearVelocity = new Vector3(0.1f, 0, 0.1f);
+		}
+	}
 
-    private void EmitBallPocketedSound()
-    {
-        var currentTime = Time.GetTicksMsec();
-        if (currentTime - _lastSoundTimeMsec < MinSoundInterval)
-            return;
+	private void EmitBallPocketedSound()
+	{
+		var currentTime = Time.GetTicksMsec();
+		if (currentTime - _lastSoundTimeMsec < MinSoundInterval)
+			return;
 
-        _lastSoundTimeMsec = currentTime;
-        BallPocketedAudio.Play();
-    }
+		_lastSoundTimeMsec = currentTime;
+		BallPocketedAudio.Play();
+	}
 }
